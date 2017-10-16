78ac58f82fb2b5d2df3e3bb2f99d3e30fe12d02d
jaki.zhang <316045346@qq.com>
2017-10-16 17:05:21 +0800

    global

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.conf import settings
 # Create your views here.
+
+def global_setting(request):
+    return {"WEB_NAME":settings.WEB_NAME,
+            "WEB_DES":settings.WEB_DES}
 
 def index(requset):
     return render(requset,'index.html',locals())
